ae263c4989754843e76d5b18ec1ca194dfe17543
EmanuelCN <emanuelghub@gmail.com>
2022-03-16 23:18:36 +0100

    MIUI: drivers/android/binder: Boost binder's priority
    Signed-off-by: EmanuelCN <emanuelghub@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -1263,14 +1263,13 @@ static void binder_transaction_priority(struct task_struct *task,
 	t->saved_priority.prio = task->normal_prio;
 
 	if (!inherit_rt && is_rt_policy(desired_prio.sched_policy)) {
-#ifndef CONFIG_MIHW
-		desired_prio.prio = NICE_TO_PRIO(0);
-#else
-		// MIUI MOD:
-		// We boost some app process to FIFO, but binder out thread
-		// from fifo has low priority, so we modify priority higher.
+		/*
+		 * MIUI MOD:
+		 * We boost some app process to FIFO, but binder out thread
+		 * from fifo has low priority, so we modify priority higher.
+		 * desired_prio.prio = NICE_TO_PRIO(0);
+		 */
 		desired_prio.prio = NICE_TO_PRIO(-10);
-#endif
 		desired_prio.sched_policy = SCHED_NORMAL;
 	}
 

9d87a2e717b7a622eea4e80e86fd01e9f7299e23
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-10-22 20:46:47 +0530

    ARM64: Add support to extract boot information from bootloader
    Change-Id: Ic265670c421d6e69dd083430de4ef9f68b862076
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- include/linux/input/qpnp-power-on.h
+++ include/linux/input/qpnp-power-on.h
@@ -55,6 +55,7 @@ enum pon_restart_reason {
 	PON_RESTART_REASON_DMVERITY_CORRUPTED	= 0x04,
 	PON_RESTART_REASON_DMVERITY_ENFORCE	= 0x05,
 	PON_RESTART_REASON_KEYS_CLEAR		= 0x06,
+	PON_RESTART_REASON_EXAID		= 0x07,
 	PON_RESTART_REASON_NORMAL		= 0x20,
 	PON_RESTART_REASON_PANIC		= 0x21
 };
@@ -69,6 +70,8 @@ bool qpnp_pon_check_hard_reset_stored(void);
 int qpnp_pon_is_lpk(void);
 int qpnp_pon_is_ps_hold_reset(void);
 int qpnp_pon_modem_pwr_off(enum pon_power_off_type type);
+int qpnp_pon_is_lpk(void);
+int qpnp_pon_is_ps_hold_reset(void);
 
 #else
 
@@ -108,6 +111,14 @@ static inline int qpnp_pon_modem_pwr_off(enum pon_power_off_type type)
 	return -ENODEV;
 }
 
+static inline int qpnp_pon_is_lpk(void)
+{
+	return -ENODEV;
+}
+static inline int qpnp_pon_is_ps_hold_reset(void)
+{
+	return -ENODEV;
+}
 #endif
 
 #endif
